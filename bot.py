ae56b989f7f142ef6e3414a259aa33e408065b44
Zerebus <jezhyip@gmail.com>
2025-03-30 22:57:18 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -21,29 +21,18 @@ HEADERS = {
     "OK-ACCESS-PASSPHRASE": OKX_PASSPHRASE
 }
 
-def get_synced_timestamp():
-    try:
-        res = requests.get("https://worldtimeapi.org/api/timezone/Etc/UTC")
-        if res.status_code == 200:
-            utc_time = res.json()["unixtime"]
-            return str(utc_time * 1000)
-        else:
-            return str(int(time.time() * 1000))
-    except:
-        return str(int(time.time() * 1000))
+def get_timestamp():
+    return str(int(time.time() * 1000))
 
 def generate_signature(timestamp, method, request_path, body=""):
     message = f"{timestamp}{method}{request_path}{body}"
     mac = hmac.new(bytes(OKX_SECRET_KEY, encoding='utf-8'),
                    bytes(message, encoding='utf-8'), hashlib.sha256)
-    d = mac.digest()
-    return base64.b64encode(d).decode()
+    return base64.b64encode(mac.digest()).decode()
 
 def place_order(signal, pair, entry, sl, tp1, tp2, risk):
-    timestamp = get_synced_timestamp()
     symbol = pair.replace("-", "/").upper()
     side = "buy" if signal == "LONG" else "sell"
-    
     notional = 376 * float(risk.strip('%')) / 100
     size = round(notional / entry, 4)
 
@@ -56,6 +45,7 @@ def place_order(signal, pair, entry, sl, tp1, tp2, risk):
     }
 
     body = json.dumps(order)
+    timestamp = get_timestamp()
     signature = generate_signature(timestamp, "POST", "/api/v5/trade/order", body)
 
     HEADERS.update({
@@ -86,5 +76,4 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=10000)
-
 
